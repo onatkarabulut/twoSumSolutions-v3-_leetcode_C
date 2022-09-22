a3645dc10448dddf7a1e0a556e2243398ef927f6
Onat <97096172+onatkarabulut@users.noreply.github.com>
2022-09-23 00:54:29 +0300

    Add files via upload
    Two Sum problem with three different version and solutions

---
+++ twoSum_v2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int *twoSum(int *numbers, int numbersSize, int target, int *returnSize) {
+  int i = 0, j = numbersSize - 1;
+  while (i < j) {
+    int ts = numbers[i] + numbers[j];
+    if (ts == target)
+      break;
+    ts > target ? j-- : i++;
+  }
+  *returnSize = 2;
+  int *ret = malloc(2 * sizeof(*ret));
+  ret[0] = i + 1;
+  ret[1] = j + 1;
+  return ret;
+}
+
+
